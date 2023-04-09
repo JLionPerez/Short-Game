dc9645cc71b0a1f1acc397b8800e789ad70ba084
Joelle Perez <j.perez.code@gmail.com>
2023-04-09 16:33:48 -0700

    working on battle function, logic failing
    Signed-off-by: Joelle Perez <j.perez.code@gmail.com>

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 # import difflib
+import random
 
+
+# global variables
 choices = ["rock", "paper", "scissors"]
 
 
 def playerMove():
-
     while True:
         choice = input("What attack do you choose? ")
         # choice = difflib.get_close_matches(choice, choices)
@@ -21,9 +23,36 @@ def playerMove():
             print("There's no option for that, please try again. ")
 
 
+def battle():
+    # playerChoice = playerMove()
+    # aiChoice = random.choice(choices)
+    playerPoints = 0
+    aiPoints = 0
+    
+    while True:
+        # if playerPoints == 3:
+        #     print("Player wins!")
+        # if aiPoints == 3:
+        #     print("AI wins!")
+        playerChoice = playerMove()
+        aiChoice = random.choice(choices)
+
+        if playerChoice == aiChoice:
+            print("It's a tie! No one gets points, keep playing!")
+        elif playerChoice == "rock" and aiChoice == "scissors":
+            playerPoints += 1
+        elif playerChoice == "scissors" and aiChoice == "paper":
+            playerPoints += 1
+        elif playerChoice == "paper" and aiChoice == "rock":
+            playerPoints += 1
+        else:
+            aiPoints += 1
+
+
 def main():
-    x = playerMove()
-    print("Player chose: ", x)
+    battle()
+    # x = playerMove()
+    # print("Player chose: ", x)
 
 
 if __name__ == "__main__":
