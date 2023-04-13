52b00001163680c4a0915b1e22eeecf91bf7c9e4
Joelle Perez <j.perez.code@gmail.com>
2023-04-13 00:22:46 -0700

    will add try again feature
    Signed-off-by: Joelle Perez <j.perez.code@gmail.com>

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 # import difflib
 import random
+import sys
 
 # global variables
 choices = ["rock", "paper", "scissors"]
 
 
+
 def playerMove():
     while True:
-        choice = input("What attack do you choose? ")
+        choice = input("What attack do you choose? ").lower()
         # choice = difflib.get_close_matches(choice, choices)
         # print(choice)
 
@@ -19,7 +21,7 @@ def playerMove():
         elif choice == "scissors":
             return "scissors"
         else:
-            print("There's no option for that, please try again. ")
+            print("There's no option for that, please try again. ", end="\n\n")
 
 
 def battle():
@@ -30,10 +32,10 @@ def battle():
 
     while True:
         if playerPoints == 3:
-            print("Player wins!")
+            print("Player wins!", end="\n\n")
             break
         if aiPoints == 3:
-            print("AI wins!")
+            print("AI wins!", end="\n\n")
             break
 
         playerChoice = playerMove()
@@ -63,9 +65,23 @@ def battle():
 
 def main():
     print("Welcome to Rock Paper Scissors!", end="\n\n")
-    battle()
-    # x = playerMove()
-    # print("Player chose: ", x)
+
+    # battle()
+
+    while True:
+        battle()
+
+        answer = input("Do you want to play again? ").lower()
+        if answer == "yes" or answer == "y":
+            continue
+        elif answer == "no" or answer == "n":
+            return False
+        else:
+            print("There's no option for that, please try again. ", end="\n\n")
+
+    # so the window doesn't suddenly dissappear after running
+    input("Press 'Enter' to exit. ")
+    sys.exit()
 
 
 if __name__ == "__main__":
